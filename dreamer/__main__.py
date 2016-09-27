926867317cf46e3882b18b621ce5f07b81a1e09f
Mouse Reeve <mousereeve@riseup.net>
2016-09-27 13:38:14 -0700

    basic test corpus

---
+++ dreamer/__main__.py
@@ -0,0 +1,22 @@
+''' generate dreams '''
+import tracery
+from tracery.modifiers import base_english
+from dreamer import corpus
+
+class Dream(object):
+    ''' define the components of a dream '''
+
+    rules = {
+        'start': '#noun.capitalize# #verb#',
+        'noun': ['cat', 'miasma'],
+        'verb': ['coagulates', 'dissapates']
+    }
+
+    def __init__(self):
+        self.grammar = tracery.Grammar(self.rules)
+        self.grammar.add_modifiers(base_english)
+
+    def dream(self):
+        ''' create a dream based on provided words '''
+        return self.grammar.flatten('#start#')
+
