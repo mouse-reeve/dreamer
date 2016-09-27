ea797e2763c97d47946acadd4d0f44031a3cdc00
Mouse Reeve <mousereeve@riseup.net>
2016-09-27 15:32:00 -0700

    Flexible grammar

--- dreamer/dream.py
+++ dreamer/dream.py
@@ -6,19 +6,47 @@ from dreamer import corpus
 class Dream(object):
     ''' define the components of a dream '''
 
-    rules = {
-        'start': '#nounpart.capitalize# #transitive_verb# #nounpart#',
-        'nounpart': '#determiner# #noun#',
-        'noun': corpus.nouns,
-        'transitive_verb': corpus.transitive_verbs,
-        'determiner': corpus.determiners
+    types = ['general', 'sex', 'flight', 'nightmare']
+    grammars = {
+        'general': '#noun.capitalize# #transitive_verb# #noun#',
+        'sex': 'You #transitive_verb# #noun#',
+        'flight': '#noun.capitalize# #transitive_verb# #noun#'
     }
 
-    def __init__(self):
+
+    def __init__(self, dream_type='general'):
+        self.rules = {
+            'noun': ['#determiner# %s' % n for n in corpus.nouns] +
+                    corpus.proper_nouns,
+            'transitive_verb': corpus.transitive_verbs,
+            'determiner': corpus.determiners
+        }
+        self.set_type(dream_type)
+
+
+    def set_type(self, dream_type):
+        ''' set the vocabulary and grammar for a given type of dream '''
+
+        # check that the provided type is known
+        if not dream_type in self.types:
+            raise KeyError('Invalid dream type. Valid types are: %s' %
+                           ', '.join(self.grammars.keys()))
+
+        self.dream_type = dream_type
+        self.rules['start'] = self.grammars[dream_type] \
+                              if dream_type in self.grammars else \
+                              self.grammars['general']
+
+        # reset tracery grammar for new patterns and words
         self.grammar = tracery.Grammar(self.rules)
         self.grammar.add_modifiers(base_english)
+
+
+    def get_type(self):
+        ''' check dream type '''
+        return self.dream_type
+
 
     def dream(self):
         ''' create a dream based on provided words '''
         return self.grammar.flatten('#start#')
-
