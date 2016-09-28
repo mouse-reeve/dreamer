1fafa4d824e9db65d1bc7dd0096a566d59b6be10
Mouse Reeve <mousereeve@riseup.net>
2016-09-28 11:27:04 -0700

    Terser corpus

--- scripts/parse_wordlist.py
+++ scripts/parse_wordlist.py
@@ -20,16 +20,22 @@ while True:
         pass
 
     # I dunno what the ? words are about
-    if freq > 100 and not '?' in lemma:
+    if freq > 100 and not '?' in lemma or '\'' in lemma:
         pos = line[2]
         try:
             json.dumps([lemma])
         except UnicodeDecodeError:
             continue
 
+        # skip parts of speech that aren't useful here
+        if pos in ['FW', 'POS', 'SYM']:
+            continue
+
         # words are already lemmatized, verb tense markers don't matter
         if re.match('^VB', pos):
             pos = 'VB'
+        if re.match('^NN.?S', pos):
+            pos = pos[0:-1]
 
         wordset[pos] = [lemma] if not pos in wordset else wordset[pos] + [lemma]
         wordset[pos] = list(set(wordset[pos]))
