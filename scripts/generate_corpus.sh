d4acd657316186076f82226d6226e07d544d0bfb
Mouse Reeve <mousereeve@riseup.net>
2016-09-28 10:54:51 -0700

    Formatted corpus file

--- scripts/generate_corpus.sh
+++ scripts/generate_corpus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-python scripts/trimwordlist.py scripts/ANC-written-lemma.txt >> dreamer/corpus/corpus.json
+python scripts/trimwordlist.py scripts/ANC-written-lemma.txt | jq . >> dreamer/corpus/corpus.json
 deactivate
