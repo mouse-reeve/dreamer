16679d17759b3e3e23c350cd66d081f7e421d9c1
Mouse Reeve <mousereeve@riseup.net>
2016-09-28 12:50:27 -0700

    Use penn tree bank pos tags out of the box

--- dreamer/__init__.py
+++ dreamer/__init__.py
@@ -1,4 +1,8 @@
 ''' initialize dreamer package '''
+import json
+from os import path
 
-from dreamer import corpus
+cwd = path.abspath(path.dirname(__file__))
+corpus = json.load(file('%s/corpus/corpus.json' % cwd))
+
 from dreamer import dream
--- dreamer/dream.py
+++ dreamer/dream.py
@@ -1,25 +1,42 @@
 ''' generate dreams '''
+from dreamer import corpus
+import re
 import tracery
 from tracery.modifiers import base_english
-from dreamer import corpus
 
 class Dream(object):
     ''' define the components of a dream '''
 
     types = ['general', 'sex', 'flight', 'nightmare']
     grammars = {
-        'general': '#noun.capitalize# #verb# #noun#',
-        'sex': 'You #verb# #noun#',
-        'flight': '#noun.capitalize# #verb# #noun#'
+        'general': [
+            '#S2#',
+            '#S2#, but then #S32#',
+            'You #VP# as #S3#',
+            'It seems like #S32#, or maybe not'],
+        'sex': '#S2#',
+        'flight': '#S2#'
     }
 
 
     def __init__(self, dream_type='general'):
         self.rules = {
-            'noun': ['#determiner# %s' % n for n in corpus.nouns] +
-                    corpus.proper_nouns,
-            'verb': corpus.verbs,
-            'determiner': corpus.determiners
+            'S2': ['You #VP# #NP#'],# 2nd-person sentence pattern
+            'S3': ['#NP# #VPZ# #NP#'],# 3rd-person sentence pattern
+            'S32': ['#NP# #VPZ# you'],# 3rd-person to 2nd-person
+            'VP': ['#RBP# #VB#'],
+            'VPZ': ['#RBP# #VBZ#'],
+            'NP': ['#DT# #JP# %s' % n for n in corpus['NN']] +
+                  ['#JP# %s' % n for n in corpus['NNP']],
+            # --- 50% chance adjective or adverb is used --- #
+            'JP': ['', '#JJ#'],
+            'RBP': ['', '#RB#'],
+            # --- straight from the corpus --- #
+            'VB': corpus['VB'],
+            'VBZ': corpus['VBZ'],
+            'DT': corpus['DT'],
+            'JJ': corpus['JJ'],
+            'RB': corpus['RB']
         }
         self.set_type(dream_type)
 
@@ -49,4 +66,12 @@ class Dream(object):
 
     def dream(self):
         ''' create a dream based on provided words '''
-        return self.grammar.flatten('#start#')
+        return self.format_dream(self.grammar.flatten('#start#'))
+
+    def format_dream(self, dream):
+        ''' remove formatting quirks '''
+        # double spaces are produced by optional adjectives
+        dream = re.sub('  ', ' ', dream)
+
+        # get the right a/an to match nouns
+        return re.sub(r' a ([aeiou])', r' an \1', dream)
